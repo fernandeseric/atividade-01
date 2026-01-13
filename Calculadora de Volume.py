34427fe3822e224d2a2f9048ec8e466efacbe4e1
fernandeseric <fernandeseric37@gmail.com>
2026-01-13 19:03:13 -0300

    Add files via upload

---
+++ Calculadora de Volume.py
@@ -0,0 +1,10 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print("Comprimento:", comprimento, "cm")
+print("Largura:", largura, "cm")
+print("Altura:", altura, "cm")
+print("Volume:", volume, "cm³")
